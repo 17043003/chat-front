5d0df1b0336ad6db5f442f94da01ca4bd49af3ad
17043003 <ishizuka533725@gmail.com>
2021-08-09 00:55:00 +0900

    show message list from server through websocket

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+import webSocket from "./components/WebSocket";
+
 function App(): JSX.Element {
+  const [messages, setMessages] = useState<string[]>([]);
+
+  webSocket.onmessage = (e) => {
+    setMessages([...messages, e.data]);
+  };
+
+  const messageItems = messages.map((message, index) => {
+    return <li key={index}>{message}</li>;
+  });
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">Chat</header>
+      {messages.length ? (
+        <ul>{messageItems}</ul>
+      ) : (
+        <p>{"メッセージはありません"}</p>
+      )}
     </div>
   );
 }
